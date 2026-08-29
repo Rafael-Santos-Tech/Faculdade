b16badc428f326bc4da7ffa7d1cd79b9436bd7dc
Rafael Dos Santos Ferreira Lima <rafasantosflima2019@gmail.com>
2026-08-29 00:03:37 -0300

    Delete Programação-de-Computadores/piramide.py

--- Programação-de-Computadores/piramide.py
+++
@@ -1,6 +0,0 @@
-# Exercicio 1
-altura = float(input("Digite o valor da altura da pirâmide: "))
-menor = float(input("Digite o valor da base menor da pirâmide: "))
-maior = float(input("Digite o valor da base maior da pirâmide: "))
-volume = (altura / 3) * (maior ** 2 + menor ** 2 + (maior ** 2 * menor ** 2) ** 0.5)
-print ("O volume do tronco da pirâmide equivale a %.2f" % volume)
